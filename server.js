006c861322a05e0814e029d5bd73b907f29f5dde
bradgamlin-sys <bradgamlin@gmail.com>
2026-03-24 17:24:55 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+let teams = [];
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.emit("updateTeams", teams);
+
+  socket.on("updateTeams", (updatedTeams) => {
+    teams = updatedTeams;
+    io.emit("updateTeams", teams);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
