a7e886903a2fb5ca0297402f6cad7a3005f07139
bradgamlin-sys <bradgamlin@gmail.com>
2026-06-30 14:27:36 -0400

    maybe oops

--- server.js
+++ server.js
@@ -7,24 +7,35 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
+
 let teams = [];
-let finalRevealCount = 0;
+
+let gameState = {
+  displayMode: "intro",
+  currentSlideIndex: 0,
+  finalRevealCount: 0,
+  podiumRevealCount: 0
+};
+
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
   socket.emit("updateTeams", teams);
+  socket.emit("gameState", gameState);
 
   socket.on("updateTeams", (updatedTeams) => {
     teams = updatedTeams;
     io.emit("updateTeams", teams);
   });
-  socket.on("finalRevealCount", (count) => {
-  finalRevealCount = count;
-  io.emit("finalRevealCount", finalRevealCount);
-});
-socket.on("podiumRevealCount", (count) => {
-  io.emit("podiumRevealCount", count);
-});
+
+  socket.on("gameState", (updatedState) => {
+    gameState = {
+      ...gameState,
+      ...updatedState
+    };
+
+    io.emit("gameState", gameState);
+  });
 });
 
 server.listen(PORT, () => {
